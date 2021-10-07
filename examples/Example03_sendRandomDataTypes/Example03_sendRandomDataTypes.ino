4296ed91e7839f6d593ebee6d581a60ec048bb96
Nick Reynolds <nick@arcanium.london>
2021-10-07 18:09:02 +0100

    Update Example 3

---
+++ examples/Example03_sendRandomDataTypes/Example03_sendRandomDataTypes.ino
@@ -0,0 +1,167 @@
+/**
+ * ESP-NOW mesh network test sketch
+ * 
+ * Author: Nick Reynolds
+ * License: Apache License v2
+ */
+
+
+#include <m2mMesh.h>
+
+uint32_t lastSend = 0;
+uint32_t sendInterval = 30000;
+uint8_t minimumArrayLength = 2;
+uint8_t maximumArrayLength = 16;
+
+void setup()
+{
+  Serial.begin(115200);
+  m2mMesh.begin();
+}
+
+void loop()
+{
+  m2mMesh.housekeeping();
+  if(millis() - lastSend > sendInterval)
+  {
+    lastSend = millis();
+    if(m2mMesh.joined())
+    {
+      Serial.printf("Flooding data to all nodes, %u bytes payload available\r\n",m2mMesh.payloadLeft());
+      uint8_t numberOfFields = random(4,13);
+      Serial.printf("\tAdding %u random fields\r\n",numberOfFields);
+      for(uint8_t count=0; count<numberOfFields;count++)
+      {
+        switch (random(0,14)){
+          case 0: //uint8_t
+            switch(random(0,2)){
+              case 0: //Single
+                addBool();
+              break;
+              case 1: //Array
+                addBoolArray();
+              break;
+            }
+          break;
+          case 1: //uint8_t
+            switch(random(0,2)){
+              case 0: //Single
+                addUint8_t();
+              break;
+              case 1: //Array
+                addUint8_tArray();
+              break;
+            }
+          break;
+          case 2: //int8_t
+            addInt8_t();
+          break;
+          case 3: //uint16_t
+            switch(random(0,2)){
+              case 0: //Single
+                addUint16_t();
+              break;
+              case 1: //Array
+                addUint16_tArray();
+              break;
+            }
+          break;
+          case 4: //int16_t
+            switch(random(0,2)){
+              case 0: //Single
+                addInt16_t();
+              break;
+              case 1: //Array
+                addInt16_tArray();
+              break;
+            }
+          break;
+          case 5: //uint32_t
+            switch(random(0,2)){
+              case 0: //Single
+                addUint32_t();
+              break;
+              case 1: //Array
+                addUint32_tArray();
+              break;
+            }
+          break;
+          case 6: //int32_t
+            switch(random(0,2)){
+              case 0: //Single
+                addInt32_t();
+              break;
+              case 1: //Array
+                addInt32_tArray();
+              break;
+            }
+          break;
+          case 7: //uint64_t
+            switch(random(0,2)){
+              case 0: //Single
+                addUint64_t();
+              break;
+              case 1: //Array
+                addUint64_tArray();
+              break;
+            }
+          break;
+          case 8: //int64_t
+            switch(random(0,2)){
+              case 0: //Single
+                addInt64_t();
+              break;
+              case 1: //Array
+                addInt64_tArray();
+              break;
+            }
+          break;
+          case 9: //float
+            switch(random(0,2)){
+              case 0: //Single
+                addFloat();
+              break;
+              case 1: //Array
+                addFloatArray();
+              break;
+            }
+          break;
+          case 10: //double
+            switch(random(0,2)){
+              case 0: //Single
+                addDouble();
+              break;
+              case 1: //Array
+                addDoubleArray();
+              break;
+            }
+          break;
+          case 11: //char
+            switch(random(0,2)){
+              case 0: //Single
+                addChar();
+              break;
+              case 1: //Array
+                addCharArray();
+              break;
+            }
+          break;
+          case 12: //C string
+            addStr();
+          break;
+          case 13: //String
+            addString();
+          break;
+        }
+      }
+      if(m2mMesh.send())
+      {
+        Serial.println("Sent successfully");
+      }
+      else
+      {
+        Serial.println("Sending failed");
+      }
+    }
+  }
+}
---
+++ examples/Example03_sendRandomDataTypes/README.md
@@ -0,0 +1,62 @@
+# Send random data types
+
+This sketch floods the mesh with messages containing a random number of fields, of random types.
+
+It serves as a demonstration of sending various data types and works well paired with Example 4 to show the mesh is working.
+
+As the number of fields and their size varies, sometimes there may not be space for all the fields chosen in the message.
+
+![](output.png)
+
+## Methods used for add data to the message
+
+The methods are templated functions that accept most built in Arduino data types, either as single items or arrays. The return value is true if there was enough space left in the message for the data. There may be space for smaller fields if a large one fails.
+
+Typically there is 220-231 bytes available in a message for data, larger items of data will have to be sent in chunks by the application.
+
+The first field of data added causes the message structure to be initialised and further fields are appended until the message is sent, cancelled, or there is no space for the field you are trying to add.
+
+```c++
+bool m2mMesh.add(<data>)
+bool m2mMesh.add(<array>, [uint8_t <arrayLength>])
+```
+
+Supported types are.
+
+```c++
+bool
+uint8_t		(unsigned short)
+uint16_t	(unsigned int)
+uint32_t	(unsigned long)
+uint64_t	(unsigned long long)
+int8_t		(short)
+int16_t		(int)
+int32_t		(long)
+int64_t		(long long)
+float		
+double
+char
+String		(Arduino Strings, not C strings)
+```
+
+There is a special pseudo-type for NULL-terminated C strings, 'str'. If you omit the array length when passing a C string to *m2mMesh.add(<data>)*, this will be used and the terminating NULL will be stripped during sending, saving a byte in the message. The NULL will be added again on retrieving the data from the field on receipt.
+
+## Methods for sending the message
+
+There is just one method to send.
+
+```c++
+bool m2mMesh.send([bool <wait>])
+```
+
+The optional 'wait' parameter is whether to wait for a result from the underlying WiFi library before returning to the user application. By default the method waits for feedback and you should normally accept this. If you do not wait and subsequently you code does heavy blocking I/O, the packet may be silently dropped.
+
+The return value is whether the packet was successfully sent or not, which does not guarantee delivery.
+
+## Other methods
+
+```c++
+uint8_t m2mMesh.payloadLeft()	//The number of bytes space left in the current message
+bool clear();					//Abandons the current message, deleting all added fields
+```
+
