0937b09e50281f838dfc2149fee6ae936a6acb4a
Nick Reynolds <nick@arcanium.london>
2021-10-12 12:08:57 +0100

    Make more consistent with other examples

--- examples/Example03_sendRandomDataTypes/Example03_sendRandomDataTypes.ino
+++ examples/Example03_sendRandomDataTypes/Example03_sendRandomDataTypes.ino
@@ -3,10 +3,16 @@
  * 
  * Author: Nick Reynolds
  * License: Apache License v2
+ * 
+ * See README.md for information
+ * 
  */
 
 
 #include <m2mMesh.h>
+bool joinedMesh = false;
+uint8_t numberOfNodes = 0;
+uint8_t numberOfReachableNodes = 0;
 
 uint32_t lastSend = 0;
 uint32_t sendInterval = 30000;
@@ -16,12 +22,40 @@ uint8_t maximumArrayLength = 16;
 void setup()
 {
   Serial.begin(115200);
-  m2mMesh.begin();
+  m2mMesh.enableDebugging(Serial,m2mMesh.MESH_UI_LOG_INFORMATION | m2mMesh.MESH_UI_LOG_WARNINGS | m2mMesh.MESH_UI_LOG_ERRORS | m2mMesh.MESH_UI_LOG_PEER_MANAGEMENT);
+  if(m2mMesh.begin())
+  {
+    Serial.print("\n\nMesh started on channel:");
+    Serial.println(WiFi.channel());
+  }
+  else
+  {
+    Serial.println("\n\nMesh failed to start");
+  }
 }
 
 void loop()
 {
   m2mMesh.housekeeping();
+  if(joinedMesh == false && m2mMesh.joined() == true)
+  {
+    joinedMesh = true;
+    Serial.println("Joined mesh");
+  }
+  else if(joinedMesh == true && m2mMesh.joined() == false)
+  {
+    joinedMesh = false;
+    Serial.println("Left mesh");
+  }
+  else if(numberOfNodes != m2mMesh.numberOfNodes() || numberOfReachableNodes != m2mMesh.numberOfReachableNodes())
+  {
+    numberOfNodes = m2mMesh.numberOfNodes();
+    numberOfReachableNodes = m2mMesh.numberOfReachableNodes();
+    Serial.print("Number of nodes:");
+    Serial.print(numberOfNodes);
+    Serial.print(", reachable nodes:");
+    Serial.println(numberOfReachableNodes);
+  }
   if(millis() - lastSend > sendInterval)
   {
     lastSend = millis();
